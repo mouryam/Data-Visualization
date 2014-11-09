7e14c09339c1390f3e17d3ae8cbbecc752d581fa
emMourya/em <emmedamourya@gmail.com/em>
2014-11-09 14:00:44 -0500

    Parse function for the data

---
+++ Parse.py
@@ -0,0 +1,56 @@
+"""
+Data Visualization Project
+Parse data from an ugly CSV or Excel file, and render it in
+JSON, save to a database, and visualize in graph form.
+Part I: Taking data from a CSV/Excel file, and return it into a format
+that is easier for Python to play with.
+
+Tutorial followed by Mourya Meda
+
+Tutorial licensing:
+Copyright (c) 2013 E. Lynn Root
+Distributed under the zlib png license. See LICENSE for details.
+"""
+
+import csv
+
+
+# Put the full path to your CSV/Excel file here
+MY_FILE = "../data/sample_sfpd_incident_all.csv"
+
+
+def parse(raw_file, delimiter):
+    """Parses a raw CSV file to a JSON-like object"""
+
+    # Open CSV file
+    opened_file = open(raw_file)
+
+    # Read CSV file
+    csv_data = csv.reader(opened_file, delimiter=delimiter)
+
+    # Build a data structure to return parsed_data
+	# Setup an empty list
+    parsed_data = []
+
+    # Skip over the first line with headers in the file
+    fields = csv_data.next()
+
+    # Iterate over each row of file, zip together field and value
+    for row in csv_data:
+    	parsed_data.append(dict(zip(fields, row)))
+
+    # Close file
+    opened_file.close()
+
+    return parsed_data
+
+def main():
+
+	#Call parse function
+	new_data = parse(MY_FILE, ",")
+
+	#Print it out
+	print new_data
+    
+if __name__ == "__main__":
+    main()
